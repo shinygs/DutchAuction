1a763505dd445eeb5dbad12b90fe5186c6f4f546
steffng <steffingsiyu0911@gmail.com>
2020-10-30 01:38:39 +0800

    changed dutch auction contract code

--- backend/contracts/DutchAuction.sol
+++ backend/contracts/DutchAuction.sol
@@ -1,133 +1,185 @@
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.0;
+import "./GLDToken.sol";
 
 contract DutchAuction {
-  address payable public organizer;
-  uint public itemsToAuction;
-  mapping (address => uint) public itemsPurchased;
-  mapping (address => uint) public amountTransferred;
-  uint public totalItemsSold;
-  uint public reservePrice;
-  uint public startPrice;
-  uint public priceDecreaseRate;
-  uint public finalPrice;
-  uint public startBlock;
-  Stages public stage;
+  //events
+  event BidSubmission(address indexed sender, uint256 amount);
 
-  event AuctionStarted();
-  event BidReceived(address _from, uint quantity);
-  event AuctionEnded(uint _finalPrice);
+  //constants
+  uint constant public MAX_TOKENS_SOLD = 20 * 10*18; //20 tokens
+  uint constant public WAITING_PERIOD = 20 minutes;
+
+  //storage
+  GLDToken public gldToken;
+  address public wallet;
+  address public owner;
+  uint public ceiling;
+  uint public priceFactor;
+  uint public startBlock;
+  uint public endTime;
+  uint public totalReceived;
+  uint public finalPrice;
+  mapping (address => uint) public bids;
 
   enum Stages {
-    StageInitialized,    // Seller has constructed the contract
-    StageAcceptingBids,  // Buyers can start bidding
-    StageBidFinished,    // Bidding period over(cost has dropped below 
-                         // reserve price) or all items sold
-    StageAuctionEnded    // Buyers can claim back extra coins sent 
+    AuctionDeployed,
+    AuctionSetUp,
+    AuctionStarted,
+    AuctionEnded,
+    TradingStarted,
+    TokensDistributed
   }
+
+  Stages public current_stage;
 
   modifier atStage(Stages _stage) {
-    require(stage == _stage);
+    require(current_stage == _stage);
     _;
   }
 
-  modifier isOrganizer() {
-    require(msg.sender == organizer);
+  struct Bid {
+    uint256 price;
+    uint256 transfer;
+    bool placed;
+    bool claimed;
+    bool isBitcoin;
+  }
+
+  mapping (address => Bid) public bids2;
+
+  modifier isOwner() {
+    require(msg.sender == owner); //only owner allowed to proceed
     _;
   }
 
-  // Constructor
-  constructor(uint _itemsToAuction, uint _reservePrice, 
-      uint _startPrice, uint _priceDecreaseRate) public { 
-    organizer = msg.sender;     
-    itemsToAuction = _itemsToAuction;
-    totalItemsSold = 0;
-    reservePrice = _reservePrice;
-    startPrice = _startPrice;
-    priceDecreaseRate = _priceDecreaseRate;
-    stage = Stages.StageInitialized;
+  modifier isWallet() {
+    require(msg.sender == wallet); //only wallet allowed to proceed
+    _;
   }
 
-  // Only organizer/ creator of the contract can allow bidding process to begin
-  function allowBidding() public isOrganizer atStage(Stages.StageInitialized)
-  {
-    stage = Stages.StageAcceptingBids;
+  modifier isValidPayLoad() {
+    require(msg.data.length > 4 && msg.data.length < 36);
+    _;
+  }
+
+  modifier timedTransactions() {
+    if(current_stage == Stages.AuctionStarted && calcTokenPrice() <= calcStopPrice()){
+      finalizeAuction();
+    }
+    if(current_stage == Stages.AuctionEnded && now > endTime + WAITING_PERIOD){
+      current_stage = Stages.TradingStarted;
+      _;
+    }
+  }
+
+  constructor(address _wallet, uint _ceiling, uint _priceFactor) public{
+    if (_wallet == 0x0 || _ceiling == 0x0 || _priceFactor == 0x0){
+      revert();
+    }
+    owner = msg.sender;
+    wallet = _wallet;
+    ceiling = _ceiling;
+    priceFactor = _priceFactor;
+    current_stage = Stages.AuctionDeployed;
+  }
+
+  function setup (address _gldToken) public isOwner atStage(Stages.AuctionDeployed){
+    if(_gldToken == address(0x0)){
+      revert();
+    }
+    gldToken = GLDToken(_gldToken);
+    //validdate token balance
+    if (GLDToken.balanceOf(_gldToken) != MAX_TOKENS_SOLD){
+      revert();
+    }
+    current_stage = Stages.AuctionSetUp;
+  }
+
+  function startAuction() public isWallet atStage(Stages.AuctionSetUp){
+    current_stage = Stages.AuctionStarted;
     startBlock = block.number;
-    emit AuctionStarted();
   }
 
-  // Get current price of the item(denoted by some decreasing function)
-  function getCurrentItemPrice() public view atStage(Stages.StageAcceptingBids) returns (uint)
-  {
-    return startPrice - priceDecreaseRate*(block.number - startBlock);
+  function changeSettings(uint _ceiling, uint _priceFactor) public isWallet atStage(Stages.AuctionSetUp){
+    ceiling = _ceiling;
+    priceFactor = _priceFactor;
   }
 
-  // Anyone can bid by sending the quantity and enough coins during bidding period
-  function bid(uint quantity) public payable atStage(Stages.StageAcceptingBids) {
-    uint currentPrice = getCurrentItemPrice();
-    // throw if we have run out of items to sell
-    require(itemsToAuction > totalItemsSold);
-    // throw if not enough coins have been received for requested number of items
-    require(currentPrice * (quantity + itemsPurchased[msg.sender]) <= 
-        amountTransferred[msg.sender] + msg.value); 
-
-    // If all items couldn't be sold by bidding period, abort the auction and let everyone
-    // withdraw back the coins they deposited 
-    if(currentPrice < reservePrice) {
-      finalPrice = 0;
-      stage = Stages.StageAuctionEnded;
-      emit AuctionEnded(0);
+  function calcCurrentTokenPrice() public timedTransactions returns (uint){
+    if(current_stage == Stages.AuctionEnded || current_stage == Stages.TradingStarted){
+      return finalPrice;
     }
-    else {
-      uint itemsToSell = quantity;
-      // If items requested is greater than number of items remaining to sell,
-      // decrease items requested to items remaining and end bidding state
-      if(totalItemsSold + itemsToSell >= itemsToAuction) {
-        itemsToSell = itemsToAuction - totalItemsSold;
-        finalPrice = currentPrice;
-        stage = Stages.StageBidFinished;
-      }
-      totalItemsSold = totalItemsSold + itemsToSell;
-      itemsPurchased[msg.sender] = itemsPurchased[msg.sender] + itemsToSell;
-    }
-    amountTransferred[msg.sender] = amountTransferred[msg.sender] + msg.value;
-    emit BidReceived(msg.sender, quantity);
+    return calcTokenPrice();
   }
 
-  // Bid organizer gets first chance to withdraw coins for items sold
-  function transferEarningsToOrganizer() public isOrganizer 
-      atStage(Stages.StageBidFinished) returns (bool success) {
-    uint earnings = totalItemsSold * finalPrice;
-    if(earnings > 0) {
-      if(organizer.send(earnings)) {
-      	stage = Stages.StageAuctionEnded;
-        emit AuctionEnded(finalPrice);
-      	return true;
-      }
-      else revert();
-    }
-    return false;
+  function updateStage() public timedTransactions returns (Stages){
+    return current_stage;
   }
 
-  // Bidders get to withdraw extra coins leftover due to their over-payments
-  function withdrawExtraCoins() public atStage(Stages.StageAuctionEnded) 
-      returns (uint amountReturned) {
-    uint totalAmountTransferred = amountTransferred[msg.sender];
-    uint extra = totalAmountTransferred - finalPrice * itemsPurchased[msg.sender];
-    if(extra > 0) {
-      amountTransferred[msg.sender] = totalAmountTransferred - extra;
-      if (!msg.sender.send(extra)) {
-          amountTransferred[msg.sender] = totalAmountTransferred;
-          return 0;
-      }
-      else {
-      	return extra;
-      }
+  function bid (address receiver) public payable isValidPayLoad timedTransactions atStage(Stages.AuctionStarted) returns (uint amount){
+    //if bid is done by ShapeShift, receiver address is set 
+    if(receiver == address(0)){
+      receiver = msg.sender;
     }
-    return 0;
+    amount = msg.value;
+    // // Prevent that more than 90% of tokens are sold. Only relevant if cap not reached.
+    uint maxWei = (MAX_TOKENS_SOLD / 10**18) * calcTokenPrice() - totalReceived;
+    // uint maxWeiBasedOnTotalReceived = ceiling - totalReceived;
+    // if (maxWeiBasedOnTotalReceived < maxWei){
+    //   maxWei = maxWeiBasedOnTotalReceived;
+    // }
+
+    // //only invest maximum possible amount
+    // if(amount > maxWei){
+    //   amount = maxWei;
+    //   // Send change back to receiver address. In case of a ShapeShift bid the user receives the change back directly.
+    //     if (!receiver.send(msg.value - amount))
+    //         // Sending failed
+    //         throw;
+    // }
+
+    // Forward funding to ether wallet
+    if (amount == 0 || !payable(wallet.address).send(amount))
+      // No amount sent or sending failed
+      revert();
+    
+    bids[receiver] += amount;
+    totalReceived += amount;
+
+    if (maxWei == amount)
+      // When maxWei is equal to the big amount the auction is ended and finalizeAuction is triggered.
+      finalizeAuction();
+
+    BidSubmission(receiver, amount);
   }
 
-  // Buyers claim the tokens/ items they were bidding for
-  function claimItems() public atStage(Stages.StageAuctionEnded) {
-    // To be implemented based on item/ token on a case-by-case basis
+  function claimTokens(address receiver) public isValidPayLoad timedTransactions atStage(Stages.TradingStarted){
+    if(receiver == 0)
+      receiver = msg.sender;
+    uint tokenCount = bids[receiver] * 10**18 / finalPrice;
+    bids[receiver] = 0;
+    gldToken.transfer(receiver, tokenCount);
+  }
+
+  function calcStopPrice() view public returns(uint){
+    return totalReceived * 10**18 / MAX_TOKENS_SOLD + 1;
+  }
+
+  function calcTokenPrice() view public returns(uint){
+    return ceiling - priceFactor*(block.number - startBlock);
+  }
+
+  function finalizeAuction() private {
+    current_stage = Stages.AuctionEnded;
+    if(totalReceived == ceiling){
+      finalPrice = calcTokenPrice();
+    } else{
+      finalPrice = calcStopPrice();
+    }
+    uint soldTokens = totalReceived * 10**18 / finalPrice;
+    // Auction contract transfers all unsold tokens to inventory multisig
+    gldToken.transfer(wallet, MAX_TOKENS_SOLD - soldTokens);
+    endTime = now;
   }
 }
