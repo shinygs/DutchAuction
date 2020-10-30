490c9460b0b56efc6822098230781ee5148be39e
steffng <steffingsiyu0911@gmail.com>
2020-10-31 04:18:23 +0800

    merged conflicts

--- webapp/the_dutch_auction/src/App.js
+++ webapp/the_dutch_auction/src/App.js
@@ -7,6 +7,7 @@ import Popup from './Popup'
 import SelectSession from "./SelectSession"
 // import { Link } from 'react-router';
 import AuctionApp from "./AuctionApp";
+import {DUTCH_AUCTION_ADDRESS,DUTCH_AUCTION_ABI} from './config'
 
 import { BrowserRouter, Route, Switch, NavLink, Link} from 'react-router-dom';
 
@@ -21,9 +22,10 @@ class App extends React.Component {
       renderSession: true,
       showPopup: false,
       current_price: "20",
-      tokens_remaining: "0"
+      tokens_remaining: "0",
+      loading: true
     };
-
+    this.getPrice = this.getPrice.bind(this)
   }
 
   async componentWillMount() {
@@ -38,8 +40,12 @@ class App extends React.Component {
     const accounts = await web3.eth.getAccounts()
     console.log(accounts)
     this.setState({ account: accounts[0] })
+    const dutchAuction = new web3.eth.Contract(DUTCH_AUCTION_ABI, DUTCH_AUCTION_ADDRESS)
+    this.setState({ dutchAuction })
     const networkId = await web3.eth.net.getId()
     console.log(networkId)
+    this.setState({ loading: false })
+    this.getPrice()
   }
   async loadWeb3() {
     if (window.ethereum) {
@@ -52,6 +58,12 @@ class App extends React.Component {
     else {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
+  }
+  async getPrice() {
+    console.log(this.state.dutchAuction)
+    const current_price = await this.state.dutchAuction.methods.calcTokenPrice().call()
+    current_price = await this.state.dutchAuction.methods.toViewablePrice(current_price).call()
+    this.setState({current_price})
   }
 
   togglePopup() {  
@@ -100,7 +112,7 @@ class App extends React.Component {
               {/* <Link to='/AuctionApp'><button onClick={onClick}>Start Auction</button></Link> */}
               
 
-              {this.state.renderSession?<SelectSession /> : <AuctionApp current_price = {this.state.current_price} remaining = {this.state.tokens_remaining}/>}
+              {this.state.loading ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div> :this.state.renderSession?<SelectSession /> : <AuctionApp getPrice={this.getPrice} current_price = {this.state.current_price} remaining = {this.state.tokens_remaining}/>}
               {/* {this.state.renderSession?<SelectSession /> : null} */}
               {/* <div id="count_down"></div> */}
               {/* <SelectSession renderme={this.state.renderSession}/> */}
---
+++ webapp/the_dutch_auction/src/config.js
@@ -0,0 +1,360 @@
+export const DUTCH_AUCTION_ADDRESS = '0xB3E5c0C2E85fCCEc95802876ef4E3199891d0829'
+
+export const DUTCH_AUCTION_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "address payable",
+        "name": "_wallet",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_startingPrice",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_priceFactor",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "sender",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "BidSubmission",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "MAX_TOKENS_SOLD",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "WAITING_PERIOD",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "bids",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "endTime",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "finalPrice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "gldToken",
+    "outputs": [
+      {
+        "internalType": "contract GLDToken",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "priceFactor",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "stage",
+    "outputs": [
+      {
+        "internalType": "enum DutchAuction.Stages",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "startBlock",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "startingPrice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalReceived",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "wallet",
+    "outputs": [
+      {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_gldToken",
+        "type": "address"
+      }
+    ],
+    "name": "setup",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "startAuction",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_startingPrice",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_priceFactor",
+        "type": "uint256"
+      }
+    ],
+    "name": "changeSettings",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "calcCurrentTokenPrice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "updateStage",
+    "outputs": [
+      {
+        "internalType": "enum DutchAuction.Stages",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address payable",
+        "name": "receiver",
+        "type": "address"
+      }
+    ],
+    "name": "bid",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "receiver",
+        "type": "address"
+      }
+    ],
+    "name": "claimTokens",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "calcStopPrice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "calcTokenPrice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "longPrice",
+        "type": "uint256"
+      }
+    ],
+    "name": "toViewablePrice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "viewablePrice",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
